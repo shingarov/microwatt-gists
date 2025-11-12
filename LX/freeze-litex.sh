ab7ba39cd3dc41caa31095c65c19ba4ed5738104
Boris Shingarov <boris@shingarov.com>
2025-11-12 05:50:57 -0500

    Use virtualenv

--- LX/freeze-litex.sh
+++ LX/freeze-litex.sh
@@ -1,21 +1,19 @@
 #/bin/bash
 
-# Get LiteX at the SHA at which DRAM worked.
+virtualenv --prompt litex .venv
+. ./.venv/bin/activate
 
-rm -rf migen
-rm -rf litex
-rm -rf litex-boards
-rm -rf liteiclink
-rm -rf litescope
-rm -rf liteeth
-rm -rf litedram
+pip install setuptools
+pip install PyYAML
+
+# Get LiteX at the SHA at which DRAM worked.
 
 git clone https://github.com/m-labs/migen.git
 cd migen
 git checkout 7bc4eb1387b39159a74c1dbd1b820728e0bfbbaa
 # for Python3.11
 git cherry-pick 0fb91737090fe45fd764ea3f71257a4c53c7a4ae
-python3 setup.py install --user
+python3 setup.py install
 cd ..
 
 
@@ -24,14 +22,14 @@ cd litex
 git checkout fb05fbc # c19c343 right after this breaks C (#1842),
                      # and c6fb9 next breaks write leveling
 git cherry-pick 19e7b53 # lx length modifier, so that C compiles
-python3 setup.py install --user
+python3 setup.py install
 cd ..
 
 
 git clone https://github.com/enjoy-digital/litedram.git
 cd litedram
 git checkout 5a114be
-python3 setup.py install --user
+python3 setup.py install
 cd ..
 
 
@@ -39,7 +37,7 @@ git clone --depth=1 https://github.com/litex-hub/litex-boards.git
 cd litex-boards
 git fetch --depth=1 origin 929e55d7e62b7430b917d8a28604f315d4bb64b5
 git reset --hard FETCH_HEAD
-python3 setup.py install --user
+python3 setup.py install
 cd ..
 
 
@@ -47,7 +45,7 @@ git clone --depth=1 https://github.com/enjoy-digital/liteiclink.git
 cd liteiclink
 git fetch --depth=1 origin 6756b4a7ba1b6474d0a4a3f8f9759baf282f9b06
 git reset --hard FETCH_HEAD
-python3 setup.py install --user
+python3 setup.py install
 cd ..
 
 
@@ -55,14 +53,14 @@ git clone --depth=1 https://github.com/enjoy-digital/litescope.git
 cd litescope
 git fetch --depth=1 origin 14e8af8dd95f96b917d25f7626e910fb1c108456
 git reset --hard FETCH_HEAD
-python3 setup.py install --user
+python3 setup.py install
 cd ..
 
 
 git clone  git@github.com:shingarov/liteeth.git
 cd liteeth
 git checkout eth2
-python3 setup.py install --user
+python3 setup.py install
 cd ..
 
 echo "Now let's generate lx cores."
